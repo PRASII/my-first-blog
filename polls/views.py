226c290c12c1109e4dae0178f91040bfe25c6fee
PRASI <prasitham@gmail.com>
2015-11-02 22:10:59 +0530

    'message'

---
+++ polls/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    #return HttpResponse(output)
+    #return HttpResponse("Hello, world. You're at the polls index.")
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
+# ...
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+	
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
